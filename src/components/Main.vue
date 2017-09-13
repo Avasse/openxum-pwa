f0d62ad708bf2ecfe7a4fda7b0d64f42183e5b96
Eric Ramat <eric.ramat@gmail.com>
2017-09-13 14:26:14 +0200

    Add empty application

---
+++ src/components/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  data () {
+    return { }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
