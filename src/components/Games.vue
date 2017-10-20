1c5e99d9bd4cca10c19335bdd4943747e4965e63
AVASSE <avasse62410@gmail.com>
2017-10-20 15:41:41 +0200

    Added logos && Modified games cards

--- src/components/Games.vue
+++ src/components/Games.vue
@@ -1,22 +1,47 @@
 <template>
   <v-container grid-list-md text-xs-center>
-    <v-layout row wrap class="mt-1">
-      <v-flex xl2 lg3 md4 sm6 xs6 v-for="(value, i) in games" :key="i">
-        <v-card height="350px">
-          <v-layout row wrap text-xs-center>
-            <v-flex xs12>
-              <v-btn :to="games[i].url" light color="warning">{{games[i].name}}</v-btn>
-            </v-flex>
-            <v-flex xs12>
-              <img :src="games[i].logo" style="max-width: 200px"></img>
-            </v-flex>
-            <v-flex xs12>
-              <v-btn :to="`/play/${games[i].gametype}`" light v-if="checkLogin">Play</v-btn>
-            </v-flex>
-          </v-layout>
-        </v-card>
-      </v-flex>
-    </v-layout>
+      <v-layout wrap>
+          <v-flex class="m-3" xs2 v-for="(game, i) in games" :key="i">
+              <v-card class="card mr-3 mb-3">
+                  <v-card-media :src="game.logo" height="200px"></v-card-media>
+                  <v-card-actions d-flex>
+                      <v-btn :to="`/play/${game.gametype}`" v-if="checkLogin" flat color="primary">
+                          Jouer
+                          <v-icon color="primary" right>
+                              play_arrow
+                          </v-icon>
+                      </v-btn>
+                      <v-btn :to="games[i].url" flat color="primary">
+                          Régles
+                          <v-icon color="primary" right>
+                            library_books
+                          </v-icon>
+                      </v-btn>
+                  </v-card-actions>
+              </v-card>
+          </v-flex>
+      </v-layout>
+
+
+
+
+      <!--<v-layout row wrap class="mt-1">-->
+      <!--<v-flex xl2 lg3 md4 sm6 xs6 v-for="(value, i) in games" :key="i">-->
+        <!--<v-card height="350px">-->
+          <!--<v-layout row wrap text-xs-center>-->
+            <!--<v-flex xs12>-->
+              <!--<v-btn :to="games[i].url" light color="warning">{{games[i].name}}</v-btn>-->
+            <!--</v-flex>-->
+            <!--<v-flex xs12>-->
+              <!--<img :src="games[i].logo" style="max-width: 200px"></img>-->
+            <!--</v-flex>-->
+            <!--<v-flex xs12>-->
+              <!--<v-btn :to="`/play/${games[i].gametype}`" light v-if="checkLogin">Play</v-btn>-->
+            <!--</v-flex>-->
+          <!--</v-layout>-->
+        <!--</v-card>-->
+      <!--</v-flex>-->
+    <!--</v-layout>-->
   </v-container>
 </template>
 
@@ -25,15 +50,15 @@
     data () {
       return {
         games: [
-          {name: 'Dvonn', logo: require('../assets/dvonn.jpg'), url: '/games/rule/dvonn', gametype: 'dvonn'},
-          {name: 'Gipf', logo: require('../assets/gipf.jpg'), url: '/games/rule/gipf'},
-          {name: 'Invers', logo: require('../assets/invers.jpg'), url: '/games/rule/invers'},
-          {name: 'Kamisado', logo: require('../assets/kamisado.jpg'), url: '/games/rule/kamisado'},
-          {name: 'Paletto', logo: require('../assets/paletto.png'), url: '/games/rule/paletto'},
-          {name: 'Pentago', logo: require('../assets/pentago.jpg'), url: '/games/rule/pentago'},
-          {name: 'Tzaar', logo: require('../assets/tzaar.jpg'), url: '/games/rule/tzaar'},
-          {name: 'Yinsh', logo: require('../assets/yinsh.jpg'), url: '/games/rule/yinsh'},
-          {name: 'Zertz', logo: require('../assets/zertz.jpg'), url: '/games/rule/zertz'}]
+          {name: 'Dvonn', logo: require('../assets/gameLogo/dvonn.jpg'), url: '/games/rule/dvonn', gametype: 'dvonn'},
+          {name: 'Gipf', logo: require('../assets/gameLogo/gipf.jpg'), url: '/games/rule/gipf'},
+          {name: 'Invers', logo: require('../assets/gameLogo/invers.jpg'), url: '/games/rule/invers'},
+          {name: 'Kamisado', logo: require('../assets/gameLogo/kamisado.jpg'), url: '/games/rule/kamisado'},
+          {name: 'Paletto', logo: require('../assets/gameLogo/paletto.jpg'), url: '/games/rule/paletto'},
+          {name: 'Pentago', logo: require('../assets/gameLogo/pentago.jpg'), url: '/games/rule/pentago'},
+          {name: 'Tzaar', logo: require('../assets/gameLogo/tzaar.jpg'), url: '/games/rule/tzaar'},
+          {name: 'Yinsh', logo: require('../assets/gameLogo/yinsh.jpg'), url: '/games/rule/yinsh'},
+          {name: 'Zertz', logo: require('../assets/gameLogo/zertz.jpg'), url: '/games/rule/zertz'}]
       }
     },
     computed: {
@@ -45,4 +70,13 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style/>
+<style scoped>
+    .card:hover {
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+        transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+    }
+
+    .card__actions {
+        justify-content: space-between;
+    }
+</style>
